289a861adfe5ed227c8572b75925190d5f9f0016
GabrielGodoy7 <142518644+GabrielGodoy7@users.noreply.github.com>
2023-11-16 21:16:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'swipe',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
